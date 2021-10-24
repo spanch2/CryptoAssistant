c14aa60a014e030f603d326e9c3232dd9a3c2fc3
Ashish Pandian <ashishpandian@berkeley.edu>
2021-10-23 18:21:06 -0700

    chat bubble

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { ChatFeed, Message } from 'react-chat-ui';
 
 export const Chat = () => {
+    const[messages, setMessages] = useState<Message[]>([]);
+    //const[is_typing, setTyping] = useState(Boolean);
+    //setTyping(true);
+    
+    const is_typing = false;
+    const messagesarray = [
+        new Message({
+          id: 1,
+          message: "I'm the recipient! (The person you're talking to)",
+        }), // Gray bubble
+        new Message({ id: 0, message: "I'm you -- the blue bubble!" }), // Blue bubble
+      ];
+      //setMessages(messagesarray)
     return (
-        <p>chat</p>
+        
+            <ChatFeed
+        messages={messagesarray} // Array: list of message objects
+        isTyping={is_typing} // Boolean: is the recipient typing
+        hasInputField={false} // Boolean: use our input, or use your own
+        showSenderName // show the name of the user who sent the message
+        bubblesCentered={false} //Boolean should the bubbles be centered in the feed?
+        // JSON: Custom bubble styles
+        bubbleStyles={
+            {
+            text: {
+                fontSize: 30
+            },
+            chatbubble: {
+                borderRadius: 70,
+                padding: 40
+            }
+            }
+        }
+        />
     )
 }
+
+export default Chat;
